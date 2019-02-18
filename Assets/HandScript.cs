a4a51c64388b718b8bb07f1da637d3b3954350b8
Elizabeth Nguyen <elizabethnguyen@DN0a24a30f.SUNet>
2019-02-17 16:30:00 -0800

    first commit

---
+++ Assets/HandScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandScript : MonoBehaviour {
+
+    Rigidbody _rb;
+
+	// Use this for initialization
+	void Start () {
+        _rb = GetComponent<Rigidbody>();
+        if(_rb)
+        {
+            _rb.velocity = Vector3.right;
+        }
+	}
+ 
+}
---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void OnTriggerEnter(Collider collision)
+    {
+        Destroy(gameObject);
+    }
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
